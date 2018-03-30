d304f403a08c1cf9ed1e2a46dbdf6c49cb0e30e0
Nolan Neeley <nolanneeley@gmail.com>
2018-03-30 07:04:50 -0500

    filled in the login view and sessions create action to

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
+
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user
+      log_in user
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  
 end
