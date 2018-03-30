111555cfcba3b03ba1302f3357e995fb134854c9
Nolan Neeley <nolanneeley@gmail.com>
2018-03-30 06:42:00 -0500

    created sessions controller with new action and named routes for new, create, and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'users/create'
@@ -6,6 +8,10 @@ Rails.application.routes.draw do
   get 'users/show'
 
  root 'welcome#index'
+
+ get    '/login',   to: 'sessions#new'
+ post   '/login',   to: 'sessions#create'
+ delete '/logout',  to: 'sessions#destroy'
 
  resources :users
 end
